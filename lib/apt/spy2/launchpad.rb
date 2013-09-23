b0c4f251379c9f151d17eee0f4448eae593a5a72
till <till@php.net>
2013-09-23 17:56:19 +0200

    Enhancement: a parser for launchpad's mirror list
     * parses the HTML output of the page
     * generates a list of (hopefully) working (!) mirrors

---
+++ lib/apt/spy2/launchpad.rb
@@ -0,0 +1,34 @@
+require 'nokogiri'
+require 'open-uri'
+
+module Apt
+  module Spy2
+    class Launchpad
+
+      def initialize(url)
+        @launchpad_url = url
+      end
+
+      def get_mirrors(country)
+
+        mirrors = []
+
+        document = Nokogiri::HTML(open(@launchpad_url))
+        document.xpath("//tr/th[text()='#{country}']/../following-sibling::*").each do |node|
+          break if node['class'] == 'head' # this is the next country heading
+
+          next if node.xpath(".//span[@class='distromirrorstatusUP']").empty? # this mirror is broken, behind, etc.
+
+          # return all mirrors: .//a[not(starts-with(@href, '/'))] - we'll just get http(s):// for now
+          node.xpath(".//a[starts-with(@href, 'http')]").each do |child|
+            mirrors << child['href']
+          end
+        end
+
+        return mirrors
+
+      end
+
+    end
+  end
+end
