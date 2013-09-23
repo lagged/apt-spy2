c8b50b8d4536a087ca2a29625c7f73e723422400
till <till@php.net>
2013-09-23 17:57:15 +0200

    Enhancement: tld to country name lookup
     * uses the iso list to get the name of a country by TLD

---
+++ lib/apt/spy2/country.rb
@@ -0,0 +1,28 @@
+module Apt
+  module Spy2
+    class Country
+
+      def initialize(country_database)
+        @database = country_database
+      end
+
+      def to_country_name(code)
+        code.upcase!
+        return code.capitalize unless code.length == 2
+
+        File.open(@database).each do |line|
+          country, tld = line.split(';', 2)
+          tld.gsub!(/\n/, '')
+
+          if code == tld
+            return country.capitalize
+          end
+
+        end
+
+        raise "Could not look up: #{code}"
+      end
+
+    end
+  end
+end
