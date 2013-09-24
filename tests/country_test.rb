bfcccd187abb8560244d34176a2db22f4d5e8322
till <till@php.net>
2013-09-24 02:19:06 +0200

    Enhancement: add another fixture

--- tests/country_test.rb
+++ tests/country_test.rb
@@ -7,13 +7,14 @@ class CountryTest < Minitest::Test
 
   def test_tld_to_name()
 
-
+    # fixtures for people who don't want to read about rails
     data = {
       'de' => 'Germany',
       'fr' => 'France',
       'IE' => 'Ireland',
       'US' => 'United States',
-      'United Kingdom' => 'United Kingdom'
+      'United Kingdom' => 'United Kingdom',
+      'germany' => 'Germany'
     }
 
     c = Apt::Spy2::Country.new(@country_list)
